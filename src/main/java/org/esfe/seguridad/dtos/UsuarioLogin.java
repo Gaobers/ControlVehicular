835b96f320b1706ee7da465736df234c07f1426d
Gabriel <gabrielobandogarcia100@gmail.com>
2026-09-24 21:06:04 -0600

    CV-30 Gabriel Obando: Implementar autenticación y gestión de usuarios con JWT

---
+++ src/main/java/org/esfe/seguridad/dtos/UsuarioLogin.java
@@ -0,0 +1,18 @@
+package org.esfe.seguridad.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioLogin {
+
+    private String correo;
+    private String clave;
+}
---
+++ src/main/java/org/esfe/seguridad/dtos/UsuarioToken.java
@@ -0,0 +1,17 @@
+package org.esfe.seguridad.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioToken {
+
+    private String token;
+}
